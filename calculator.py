02406e38234162204c36ecce8bcf20042959cca0
Kingsley Ihemere <dekings44@gmail.com>
2022-06-07 00:46:53 +0100

    My calculator project

--- calculator.py
+++ calculator.py
@@ -16,12 +16,12 @@ def subtract(n1, n2):
 
 # Multiplication
 def multiply(n1, n2):
-    n1 * n2
+    return n1 * n2
 
 
 # Divide
 def divide(n1, n2):
-    n1 / n2
+    return n1 / n2
 
 
 signs = {
@@ -32,7 +32,15 @@ signs = {
     }
 
 num1 = int(input('What is the first number?\n'))
-num2 = int(input('What is the second number?\n'))
+
 
 for symbol in signs:
     print(symbol)
+
+sign_symbol = input('Please pick a sign from the above\n')
+
+num2 = int(input('What is the second number?\n'))
+calculation_function = signs[sign_symbol]
+answer = calculation_function(num1, num2)
+
+print(f'{num1} {sign_symbol} {num2} = {answer}')
