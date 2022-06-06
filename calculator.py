d7876c141a3e19c2414fd7417bd374b19fbf37c7
Kingsley Ihemere <dekings44@gmail.com>
2022-06-07 00:29:02 +0100

    My calculator project

--- calculator.py
+++ calculator.py
@@ -25,8 +25,14 @@ def divide(n1, n2):
 
 
 signs = {
-    '+': add(),
-    '-': subtract(),
-    '*': multiply(),
-    '/': subtract()
+    '+': add,
+    '-': subtract,
+    '*': multiply,
+    '/': subtract
     }
+
+num1 = int(input('What is the first number?\n'))
+num2 = int(input('What is the second number?\n'))
+
+for symbol in signs:
+    print(symbol)
