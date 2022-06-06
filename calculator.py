026a901c0de17aa7b7aa4b55cc1ef7cf7bbe02b3
Kingsley Ihemere <dekings44@gmail.com>
2022-06-06 22:50:58 +0100

    My calculator project

--- calculator.py
+++ calculator.py
@@ -4,7 +4,24 @@
 
 #Calculator
 
+
+# Add
 def add(n1, n2):
     return n1 + n2
 
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
 
+
+# Multiplication
+def multiply(n1, n2):
+    n1 * n2
+
+
+# Divide
+def divide(n1, n2):
+    n1 / n2
+
+
+signs = {}
