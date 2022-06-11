ff8ec7b17bf90c42f6ed048dd1c5269fee68f041
Kingsley Ihemere <dekings44@gmail.com>
2022-06-11 22:39:50 +0100

    updated calculator

--- calculator.py
+++ calculator.py
@@ -37,18 +37,20 @@ num1 = int(input('What is the first number?\n'))
 for symbol in signs:
     print(symbol)
 
-sign_symbol = input('Please pick a sign from the above\n')
 
-num2 = int(input('What is the second number?\n'))
-calculation_function = signs[sign_symbol]
-answer = calculation_function(num1, num2)
+should_continue = True
 
-print(f'{num1} {sign_symbol} {num2} = {answer}')
+while should_continue:
 
-sign_symbol = input('Please pick a sign from the above\n')
+    sign_symbol = input('Please pick a sign from the above\n')
 
-num3 = int(input('What is the next number?\n'))
-calculation_function = signs[sign_symbol]
-answer2 = calculation_function(calculation_function(num1, num2), num3)
+    num2 = int(input('What is the second number?\n'))
+    calculation_function = signs[sign_symbol]
+    answer = calculation_function(num1, num2)
 
-print(f'{answer} {sign_symbol} {num3} = {answer2}')
+    print(f'{num1} {sign_symbol} {num2} = {answer}')
+
+    if input(f"Type 'y' to continue calculating with {answer}: ") == "y":
+        num1 = answer
+    else:
+        should_continue = False
