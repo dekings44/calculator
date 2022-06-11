6b79256476c4df2e196b0ed0a637e238486fea08
Kingsley Ihemere <dekings44@gmail.com>
2022-06-11 22:23:36 +0100

    updated calculator

--- calculator.py
+++ calculator.py
@@ -44,3 +44,11 @@ calculation_function = signs[sign_symbol]
 answer = calculation_function(num1, num2)
 
 print(f'{num1} {sign_symbol} {num2} = {answer}')
+
+sign_symbol = input('Please pick a sign from the above\n')
+
+num3 = int(input('What is the next number?\n'))
+calculation_function = signs[sign_symbol]
+answer2 = calculation_function(calculation_function(num1, num2), num3)
+
+print(f'{answer} {sign_symbol} {num3} = {answer2}')
